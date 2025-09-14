150de45decc5fdf47bf721a8cb075cdc385bb8df
K-Maheswar-reddy <maheshreddy77570k@gmail.com>
2025-09-14 14:33:07 +0530

    Update config.js
    added SERVER_URL ( render "backend" URL to the front end )

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    "SERVER_URL": "http://localhost:3000", //backend url
+    "SERVER_URL": "https://portfolio-backend-kg67.onrender.com", //backend url
     "THEME_COLORS":{
         PRIMARY: "#007bff",
         SECONDARY: "#fc3"
