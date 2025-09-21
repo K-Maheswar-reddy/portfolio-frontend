fa3e347a253e4061bc52c710bd1d45bd487998e6
K-Maheswar-reddy <maheshreddy77570k@gmail.com>
2025-09-21 11:27:38 +0530

    Update NavBar.jsx
    changed font size

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -19,15 +19,18 @@ const NavBar = () => {
         }
     };
 
-    return (
+        return (
         <div className="fixed bottom-8 w-full bg-transparent text-white z-50">
-            <div className="max-w-3xl mx-auto flex justify-center bg-black/20 backdrop-blur-sm space-x-8 py-3 rounded-full border border-white/10 shadow-lg">
+            <div className="max-w-3xl mx-auto flex justify-center bg-black/20 backdrop-blur-sm space-x-4 py-3 rounded-full border border-white/10 shadow-lg 
+                sm:space-x-8
+            ">
                 {menuItems.map(items => ( 
                     <button
                         key={items.id}
                         onClick={() => scrollToSection(items.id)}
-                        className="transition-colors hover:bg-slate-100/60 py-2 px-4 duration-300 rounded-full hover:text-black hover:text-semibold text-md"
-                    >
+                        className="transition-colors hover:bg-slate-100/60 py-2 px-2 duration-300 rounded-full hover:text-black hover:text-semibold text-sm  
+                        sm:text-md sm:px-4  
+                    ">
                         {items.label}
                     </button>
                 ))}
