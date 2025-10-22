36133fb71faad8d8aee2d75e9d1058d98f299ff8
K-Maheswar-reddy <maheshreddy77570k@gmail.com>
2025-10-22 07:50:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SkillsDisplay from './components/Skills';
 import { DataProvider, useData } from './context/DataContext';
 import { AiOutlineLoading } from 'react-icons/ai';
 import { BiErrorCircle } from 'react-icons/bi';
+import DownloadResume from "./DownloadResume";
 
 function AppContent() {
   const { loading, error } = useData();
@@ -45,6 +46,7 @@ function AppContent() {
 
       <div id='about'>
         <AboutMe />
+        <DownloadResume />
       </div>
 
       <div id='experience'>
