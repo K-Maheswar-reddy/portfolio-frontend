0222e8e229656e0a9536ae82252280bebeece6a4
K-Maheswar-reddy <maheshreddy77570k@gmail.com>
2025-10-22 07:34:51 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,6 +7,14 @@ const SkillsDisplay = () => {
     return (
         <div className="bg-slate-950 text-white font-mono py-8">
             <div className="mx-20">
+                    <a
+                      href="/resume.pdf"
+                      download="Mahe_K_Resume.pdf"
+                      className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-2xl shadow hover:bg-blue-700 transition-all"
+                    >
+                      <Download size={20} />
+                      Download Resume
+                    </a>
                 <h2 className=" text-4xl">Technical Skills</h2>
                 <p className="my-2 text-lg text-gray-400">My technical toolkit and proficiencies.</p>
                 {skill.map((skills, index) => (
